710a31313a502cefc42650ad2a3cb1270bde98fa
MrLiam2614 <liamcominotti@gmail.com>
2024-06-14 17:25:32 +0200

    Changes to MrLiam2614 API

--- velocity/src/main/java/net/mrliam2614/mrliamapi/commands/commands/CommandMeta.java
+++ velocity/src/main/java/net/mrliam2614/mrliamapi/commands/commands/CommandMeta.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Target;
 public @interface CommandMeta {
     String name();
 
-    String permission() default "velocitychatmanager.default";
+    String permission() default "";
 
     String description() default "";
 
